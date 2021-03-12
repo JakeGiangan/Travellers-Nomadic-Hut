72f4a132ef6a6b4244894663fee8d8d0802739c6
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-03-12 16:49:30 +0800

    [TNH_004]guest/renter_signup_login_profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,8 @@
 class UsersController < ApplicationController
-  def new
-      @user = User.new
+  before_action :user_signed_in?
+
+  def show
+    @user = User.find(params[:id])
   end
 
-  def create
-  @user = User.new(user_params)
-      if @user.save
-      flash[:success] = "Sign-up Success!"
-      redirect_to root_url
-      else
-      flash[:warning] = "Sign-up Failed"
-      render 'new'
-      end
-  end
-
-private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :birthdate, :languages, :email, :contact_number, :account_type, :password,
-    :password_confirmation)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users 
 
   resources :dashboard
+  resources :users
   root 'static_pages#index'
 end
