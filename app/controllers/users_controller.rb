53ccddb979c8ad3d398067f039d9fdacb5d2743e
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-03-15 13:27:04 +0800

    made changes based from comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
   def new
-      @user = User.new
+    @user = User.new
   end
 
   def create
   @user = User.new(user_params)
-      if @user.save
+    if @user.save
       flash[:success] = "Sign-up Success!"
       redirect_to root_url
-      else
+    else
       flash[:warning] = "Sign-up Failed"
       render 'new'
-      end
+    end
   end
 
 private
