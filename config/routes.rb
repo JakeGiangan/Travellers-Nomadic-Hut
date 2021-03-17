cf2a781177a36d8fb9d75443837ad49c0de19fac
JakeGiangan <73863408+JakeGiangan@users.noreply.github.com>
2021-03-17 15:00:56 +0800

    [TNH_003]host-dashboard (#3)
    * [TNH_002]host-signup-login
    
    * converted to use Devise Gem
    
    * [TNH_003]host-dashboard
    
    * corrected the navigation bar implementation
    
    * rebased from develop
    
    * made changes based from comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users 
 
-  root 'static_pages#index'
+  resources :dashboard
+  get '/hosting-dashboard', to: 'dashboard#dashboard'
+  root 'static_pages#home'
   resources :users
 end
