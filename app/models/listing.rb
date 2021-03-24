2c0a21c11437a0db7ae35f05286af2dfb776722d
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-03-24 17:47:08 +0800

    updated listing image header

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,3 @@
 class Listing < ApplicationRecord
   has_many :images
-  validate :image_type
-
-  def image_type
-    images.each do |image|
-      if !image.content_type.in?(%('image/jpeg image/png'))
-        errors.add(:images, 'needs to be a JPEG or PNG')
-      end
-    end
-  end
 end
