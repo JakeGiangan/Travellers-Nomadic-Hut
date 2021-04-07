bdbd10d3953cec7a3a2527679f2a60c155e83c5d
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-04-07 21:46:16 +0800

    TNH_012-update-search-page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def search
     session[:address] = params[:search] if params.has_key?(:search)
-    @search = Listing.where(is_active: true)
-    @search = @search.near("#{session[:address]}", 5, order: 'distance')
+    @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
+                       .pluck(:listing_id)
+    @search = Listing.where(is_active: true).where.not(id: @bookings).near("#{session[:address]}", 5, order: 'distance')
     @search = @search.ransack(params[:q])
     @listing_list = @search.result(distinct: true).paginate(page: params[:page], per_page: 2)
   end
