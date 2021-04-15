6ad52e89923e3504efb42bebc3baee1655565ab9
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-04-15 21:07:41 +0800

    made changes based from comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def search
     session[:address] = params[:search] if params.has_key?(:search)
     @search = Listing.where(is_active: true)
-    @search = @search.where(address: session[:address])
+    @search = @search.near("#{session[:address]}", 5, order: 'distance')
     @search = @search.ransack(params[:q])
     @listing_list = @search.result(distinct: true).paginate(page: params[:page], per_page: 2)
   end
