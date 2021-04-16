b8aad4652794698bdd3005546036afdde45ed041
JakeGiangan <73863408+JakeGiangan@users.noreply.github.com>
2021-04-16 16:27:00 +0800

    [TNH_011]-google-maps (#11)
    * [TNH_011]-google-maps
    
    * rebased from develop
    
    * made changes based from comments
    
    * added maps in the search page
    
    * made changes based from comments
    
    * fixed the issue on maps not loading

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,8 @@ class SearchController < ApplicationController
   def search
     session[:address] = params[:search] if params.has_key?(:search)
     @search = Listing.where(is_active: true)
-    @search = @search.where(address: session[:address])
+    @search = @search.near("#{session[:address]}", 5, order: 'distance')
     @search = @search.ransack(params[:q])
-    @listing_list = @search.result(distinct: true).paginate(page: params[:page], per_page: 10)
+    @listing_list = @search.result(distinct: true).paginate(page: params[:page], per_page: 2)
   end
 end
