2ed73e068883b377be37558fab27b034c676460d
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-04-19 15:57:48 +0800

    fixed missing checkout search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def search
     session[:address] = params[:search] if params.has_key?(:search)
-    @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
+    @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}' AND check_out_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
                        .pluck(:listing_id)
     @search = Listing.where(is_active: true).where.not(id: @bookings).near("#{session[:address]}", 5, order: 'distance')
     @search = @search.ransack(params[:q])
