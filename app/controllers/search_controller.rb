32909b58e99982e12fb3817744c31c4e844aec88
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-04-20 14:48:54 +0800

    added search all logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,21 @@
 class SearchController < ApplicationController
   def search
     session[:address] = params[:search] if params.has_key?(:search)
-    @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'
-                            AND check_out_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
-                       .pluck(:listing_id)
-    @search = Listing.where(is_active: true).where.not(id: @bookings).near("#{session[:address]}", 5, order: 'distance')
+    if session[:address].present? && session[:address].empty? == false
+      @search = Listing.where(is_active: true)
+      if params[:check_in_date].present? && params[:check_in_date].empty? == false && params[:check_out_date].present? && params[:check_out_date].empty? == false 
+        @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'
+                AND check_out_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
+          .pluck(:listing_id)
+        @search = Listing.where(is_active: true).where.not(id: @bookings)
+      end
+    else
+      @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'
+                                AND check_out_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
+                          .pluck(:listing_id)
+      @search = Listing.where(is_active: true).where.not(id: @bookings).near("#{session[:address]}", 5, order: 'distance')
+    end
+
     @search = @search.ransack(params[:q])
     @listing_list = @search.result(distinct: true).paginate(page: params[:page], per_page: 2)
   end
