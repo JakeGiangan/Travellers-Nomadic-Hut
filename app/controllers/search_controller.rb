a8974451c262a9cb65d9c5d8ea46462655327090
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-04-21 13:58:17 +0800

    made changes based from comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,9 @@
 class SearchController < ApplicationController
+  include SearchHelper
   def search
     session[:address] = params[:search] if params.has_key?(:search)
-    if session[:address].present? && session[:address].empty? == false
+    if check_if_filled?(session[:address])
       @search = Listing.where(is_active: true)
-      if params[:check_in_date].present? && params[:check_in_date].empty? == false && params[:check_out_date].present? && params[:check_out_date].empty? == false 
-        @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'
-                AND check_out_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
-          .pluck(:listing_id)
-        @search = Listing.where(is_active: true).where.not(id: @bookings)
-      end
     else
       @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'
                                 AND check_out_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
@@ -16,6 +11,12 @@ class SearchController < ApplicationController
       @search = Listing.where(is_active: true).where.not(id: @bookings).near("#{session[:address]}", 5, order: 'distance')
     end
 
+    if check_if_filled?(params[:check_in_date]) && check_if_filled?(params[:check_out_date])
+      @bookings = Booking.where("check_in_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'
+                                AND check_out_date BETWEEN '#{params[:check_in_date]}' AND '#{params[:check_out_date]}'")
+                        .pluck(:listing_id)
+      @search = Listing.where(is_active: true).where.not(id: @bookings)
+    end
     @search = @search.ransack(params[:q])
     @listing_list = @search.result(distinct: true).paginate(page: params[:page], per_page: 2)
   end
