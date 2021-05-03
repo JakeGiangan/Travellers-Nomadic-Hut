764680bbcf863a5092f6fa688090c9e930b2fadd
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2021-05-03 21:45:21 +0800

    updated the dashboard counters

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   def dashboard
+    @users_listings = current_user.listings
+    @bookings = Booking.where(id: @users_listings)
+    @booked = @bookings.count
+    @completed = Review.where(booking_id: @bookings).count
   end
     
 end
